750e1b07f8cbb9845b743aaf6e3ff78453ee8550
abhixyz1 <143863755+abhixyz1@users.noreply.github.com>
2023-09-26 08:58:13 +0700

    Add files via upload

---
+++ myfirstjava20.java
@@ -0,0 +1,8 @@
+public class myfirstjava20 {
+    public static void main(String[] args) {
+        int w = 1732;
+        double we = w;
+        System.out.println(w);
+        System.out.println(we);
+    }
+}
